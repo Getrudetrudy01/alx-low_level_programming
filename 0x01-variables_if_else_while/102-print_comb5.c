d22e9c0e5dc29c15d0d4a4d87b2e24f58fde9168
Getrudetrudy01 <getrudemakumator@gmail.com>
2022-04-09 10:55:18 +0100

    corrected all errors

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,27 +1,51 @@
 #include <stdio.h>
-
 /**
- * main - prints the numbers from 00 to 99
- * Return: Always 0 (success)
+ * main - main block
+ * Description: Print all possible combinations of two two-digit numbers.
+ * Numbers should range from 0 to 99.
+ * The two numbers should be separated by a space.
+ * All numbers should be printed with two digits. 1 should be printed as 01.
+ * Combination of numbers must be separated by a comma followed by a space.
+ * Combinations of numbers should be printed in ascending order.
+ * `00 01` and `01 00` are considered as the same combination.
+ * You can only use `putchar` to print to console.
+ * You can only use `putchar` up to 8 times.
+ * You are not allowed to use any variable of type `char`.
+ * Return: 0
  */
 int main(void)
 {
-	int n, m;
+	int i, j;
+	int a, b, c, d;
 
-	for (n = 48; n <= 57; n++)
+	for (i = 0; i < 100; i++)
 	{
-		for (m = 48; m <= 57; m++)
+		a = i / 10; /* doubles fnum */
+		b = i % 10; /* singles fnum */
+
+		for (j = 0; j < 100; j++)
 		{
-			putchar(n);
-			putchar(m);
-			if (n != 57 || m != 57)
+			c = j / 10; /* doubles snum */
+			d = j % 10; /* singles snum */
+
+			if (a < c || (a == c && b < d))
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(32);
+				putchar(c + '0');
+				putchar(d + '0');
+
+				if (!(a == 9 && b == 8))
+				{
+					putchar(44);
+					putchar(32);
+				}
 			}
 		}
 	}
-	putchar('\n');
+	putchar(10);
+
 	return (0);
 }
 
